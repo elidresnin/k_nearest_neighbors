bd0996a72e23ec10d9395ab3370b4b7769529701
Eli Dresnin <eddres229@gmail.com>
2025-04-08 11:15:02 -0400

    test

--- DigitMatcherRunner.py
+++ DigitMatcherRunner.py
@@ -34,7 +34,7 @@ class DigitMatcherRunner:
     def main():
         # Test Activity 2
         print("Activity 2 - Read digits from an input file")
-        digit_collection = DigitMatcher("small_train.csv")
+        digit_collection = DigitMatcher("tiny_train.csv")
         #print(digit_collection.get_digits()[0])
 
         # Testing Activity 3
@@ -46,8 +46,8 @@ class DigitMatcherRunner:
         #print(secondDigit)
 
         # Get test digits
-        test_digits = DigitMatcherRunner.populate_array_of_test_digits("small_test.csv")
-        testDigit = test_digits[2]
+        test_digits = DigitMatcherRunner.populate_array_of_test_digits("tiny_test.csv")
+        testDigit = test_digits[0]
 
         # Testing Activity 4
         print("Activity 4 - Find most similar")
@@ -60,7 +60,7 @@ class DigitMatcherRunner:
         kNN = digit_collection.find_k_most_similar(k)
         print("kNN digit's label is " + str(digit_collection.k_nearest_neighbors(k)))
         PrintHelper.print(firstDigit, kNN)
-        print(digit_collection.k_nearest_neighbors(3))
+        # print(digit_collection.k_nearest_neighbors(3))
 
 
         print("Activity 6 - Weighted kNN")
@@ -68,7 +68,7 @@ class DigitMatcherRunner:
         PrintHelper.print(firstDigit, kNN)
 
         print("\nActivity 7 - Test Accuracy")
-        k = 3
+
         correct_predictions = 0
         total_tests = len(test_digits)
 
@@ -78,9 +78,14 @@ class DigitMatcherRunner:
             true_label = test_digit.get_label()
             if predicted_label == true_label:
                 correct_predictions += 1
+            else:
+                print("guessed label:" + str(predicted_label))
+                print("correct label:" + str(true_label))
 
         accuracy = correct_predictions / total_tests
-        print("\nActivity 7 - Test Accuracy")
+        print("Number of correct: " + str(correct_predictions))
+        print("Number of incorrect: " + str(total_tests - correct_predictions))
+
         print(f"Accuracy using k = {k}: {accuracy:.2%}")
 
 if __name__ == "__main__":
